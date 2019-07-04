1ceac1f80d865bfebb74bc58bcb6e69021431145
Arthur_Casona_New <artlufo@gmail.com>
2019-07-04 20:18:18 -0300

    add graphics plot (not working)

---
+++ graf.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+f=open("data.txt","r")
+
+v1=[]
+v2=[]
+v3=[]
+for i in range(0,240):
+    a=f.readline()
+    valor1,valor2,valor3 = a.split()
+    if float(valor3)==1:
+        v1.append(float(valor1))
+        v2.append(float(valor2))
+        v3.append(float(valor3))
+
+print(v1)
+plt.scatter(v2, v1)
+plt.ylim(0,3)
+plt.xlim(0,0.0016)
+plt.show()
