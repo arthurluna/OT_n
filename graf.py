676140cad943c1be94114518f4594e51db14d4b8
Arthur_Casona_New <artlufo@gmail.com>
2019-07-05 03:16:12 -0300

    added grids and labels to plot

--- graf.py
+++ graf.py
@@ -15,7 +15,10 @@ for i in range(0,1680):
         v3.append(float(valor3))
 
 print(v1)
+fig,ax=plt.subplots()
 plt.scatter(v2, v1)
 plt.ylim(0,3)
 plt.xlim(0,0.0200)
+plt.grid(True)
+ax.set(xlabel='$Re(n_1)$',ylabel='$Im(n_1)$',title='Região de pinçamento da microesfera \n em função do seu índice de refração $n_1$ \n $raio = 0.5\mu m $ , n_{água}=1.332')
 plt.show()
